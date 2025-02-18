bf1df285fab3f52562184d3e2fe7179b4160075b
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-02-18 04:39:21 -0300

    feat: add svelte
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-import './style.css'
+import './app.css'
+import {mount} from "svelte"
+import App from "./App.svelte"
+
+const app = mount(App, {
+    target: document.getElementById("app")!
+})
+
+export default app
