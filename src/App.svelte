d7ca125c2af024dee37410dbd96bd00edd1e1458
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-03 12:16:52 -0300

    feat: add accessibility attributes to social media links and update package.json scripts
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -10,14 +10,14 @@
     <div class="social-networks">
         <span>Follow me at social networks</span>
         <nav>
-            <a target="_blank" href="https://linkedin.com/in/untalsanders">
+            <a aria-labelledby="Shape" target="_blank" href="https://linkedin.com/in/untalsanders">
                 <svg fill="#000000" height="64px" width="64px"
                      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      viewBox="0 0 504.4 504.4" xml:space="preserve" stroke="#000000">
                     <path d="M377.6,0.2H126.4C56.8,0.2,0,57,0,126.6v251.6c0,69.2,56.8,126,126.4,126H378c69.6,0,126.4-56.8,126.4-126.4V126.6 C504,57,447.2,0.2,377.6,0.2z M168,408.2H96v-208h72V408.2z M131.6,168.2c-20.4,0-36.8-16.4-36.8-36.8c0-20.4,16.4-36.8,36.8-36.8 c20.4,0,36.8,16.4,36.8,36.8C168,151.8,151.6,168.2,131.6,168.2z M408.4,408.2H408h-60V307.4c0-24.4-3.2-55.6-36.4-55.6 c-34,0-39.6,26.4-39.6,54v102.4h-60v-208h56v28h1.6c8.8-16,29.2-28.4,61.2-28.4c66,0,77.6,38,77.6,94.4V408.2z"></path>
                 </svg>
             </a>
-            <a target="_blank" href="https://youtube.com/@untalsanders">
+            <a aria-labelledby="Shape" target="_blank" href="https://youtube.com/@untalsanders">
                 <svg fill="#000000" height="64px" width="64px"
                      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      viewBox="0 0 504 504" xml:space="preserve">
@@ -25,14 +25,14 @@
                     <path d="M377.6,0H126C56.8,0,0,56.8,0,126.4V378c0,69.2,56.8,126,126,126h251.6c69.6,0,126.4-56.8,126.4-126.4V126.4 C504,56.8,447.2,0,377.6,0z M408,264.4c0,26.4-2.4,53.2-2.4,53.2s-2.8,22.4-12,32.4c-12,13.2-25.2,13.2-31.2,14 c-44,3.2-110,3.6-110,3.6s-82-1.2-107.2-3.6c-6.8-1.2-22.8-0.8-34.8-14c-9.6-10-12-32.4-12-32.4S96,290.8,96,264.4v-24.8 c0-26.4,2.4-53.2,2.4-53.2s2.8-22.4,12-32.4c12-13.2,25.2-13.6,31.2-14.4C186,136.4,252,136,252,136s66,0.4,110,3.6 c6,0.8,19.6,1.2,31.6,14c9.6,10,12,32.8,12,32.8s2.4,26.8,2.4,53.2V264.4z"></path>
                 </svg>
             </a>
-            <a target="_blank" href="https://x.com/@untalsanders">
+            <a aria-labelledby="Shape" target="_blank" href="https://x.com/@untalsanders">
                 <svg fill="#000000" height="64px" width="64px"
                      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      viewBox="0 0 504.4 504.4" xml:space="preserve">
                     <path d="M377.6,0.2H126.4C56.8,0.2,0,57,0,126.6v251.6c0,69.2,56.8,126,126.4,126H378c69.6,0,126.4-56.8,126.4-126.4V126.6 C504,57,447.2,0.2,377.6,0.2z M377.2,189c0,2.8,0,5.6,0,8.4c0,84-64.8,180.8-183.6,180.8c-36.4,0-70.4-10.4-98.8-28.4 c5.2,0.4,10,0.8,15.2,0.8c30.4,0,58-10,80-27.2c-28.4-0.4-52-18.8-60.4-44c4,0.8,8,1.2,12,1.2c6,0,12-0.8,17.2-2.4 c-28.8-6-50.8-31.6-50.8-62.4V215c8,4.8,18.4,7.6,28.8,8c-17.2-11.2-28.8-30.8-28.8-52.8c0-11.6,3.2-22.4,8.8-32 c32,38.4,79.2,63.6,132.8,66.4c-1.2-4.8-1.6-9.6-1.6-14.4c0-35.2,28.8-63.6,64.4-63.6c18.4,0,35.2,7.6,47.2,20 c14.8-2.8,28.4-8,40.8-15.6c-4.8,14.8-15.2,27.2-28.4,35.2c13.2-1.6,25.6-4.8,37.2-10C400.4,169,389.6,180.2,377.2,189z"></path>
                 </svg>
             </a>
-            <a target="_blank" href="https://instagram.com/untalsanders">
+            <a aria-labelledby="Shape" target="_blank" href="https://instagram.com/untalsanders">
                 <svg fill="#000000" height="64px" width="64px"
                      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      viewBox="0 0 504.4 504.4" xml:space="preserve">
@@ -42,7 +42,7 @@
                     <path d="M377.6,0.2H126.4C56.8,0.2,0,57,0,126.6v251.6c0,69.2,56.8,126,126.4,126H378c69.6,0,126.4-56.8,126.4-126.4V126.6 C504,57,447.2,0.2,377.6,0.2z M408,219.8L408,219.8l0,128.8c0,33.6-26,59.6-59.6,59.6H155.6c-33.6,0-59.6-26-59.6-59.6V219.8v-64 c0-33.6,26-59.6,59.6-59.6h192.8c33.6,0,59.6,26,59.6,59.6V219.8z"></path>
                 </svg>
             </a>
-            <a target="_blank" href="HTTPS://github.com/untalsanders">
+            <a aria-labelledby="Shape" target="_blank" href="HTTPS://github.com/untalsanders">
                 <svg fill="#000000" height="64px" width="64px"
                      viewBox="0 0 73 73" xmlns="http://www.w3.org/2000/svg"
                      xmlns:xlink="http://www.w3.org/1999/xlink">
